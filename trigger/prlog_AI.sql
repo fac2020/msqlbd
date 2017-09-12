931d9592c04c26120862cc569e5b3801d8921b28
fac2020 <facquodvis@inbox.ru>
2017-09-12 10:27:34 +0400

    Завершение автозадач по смене статуса

--- trigger/prlog_AI.sql
+++ trigger/prlog_AI.sql
@@ -1,6 +1,8 @@
 DROP TRIGGER IF EXISTS `prlog_AI`;
 DELIMITER $$
 CREATE TRIGGER `prlog_AI` AFTER INSERT ON `prlog` FOR EACH ROW BEGIN
- -- Создаём событие "Добавление заявки Пр"
+ -- Завершаем автозадачи, связанные с этой заявкой.
+ UPDATE task t set t.ISCOMPLETE=1 where t.ID = NEW.PRID and t.AGENCYID=NEW.AGENCYID;
+ -- Создаём событие "Смена статуса заявки Пр"
  call ADD_EVENT(18,NEW.ID,'',null);
 END
