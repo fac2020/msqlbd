6e782df9c9d9a32903b7d3dcaf4016a60ccec6f6
fac2020 <facquodvis@inbox.ru>
2017-09-25 09:53:44 +0400

    fix для функции количества показов
    добавлен диаппазон дат

--- function/GET_POKAZ4ZAKAZPR.sql
+++ function/GET_POKAZ4ZAKAZPR.sql
@@ -1,10 +1,17 @@
+DROP FUNCTION IF EXISTS GET_POKAZ4ZAKAZPR; 
 DELIMITER $$
-CREATE DEFINER=`stastrjn_nrn`@`localhost` FUNCTION `GET_POKAZ4ZAKAZPR`(`ZAKAZPR` INT, `POKAZSTATE` TINYINT) RETURNS int(11)
+CREATE DEFINER=`stastrjn_nrn`@`localhost` FUNCTION `GET_POKAZ4ZAKAZPR`(`ZAKAZPR` INT, `POKAZSTATE` TINYINT, `FROMDATE` DATE, `TODATE` DATE) RETURNS int(11)
     COMMENT 'Возвращает количество показов по заявке на продажу'
 BEGIN
   DECLARE iResult int;
-  SELECT count(wp.ID) INTO iResult FROM pokaz wp where wp.PRID = ZAKAZPR and wp.STATE = POKAZSTATE;
-  IF (iResult is null) then SET iResult =-1; END IF;
+  DECLARE dSTART date;
+  DECLARE dEND date;
+  SELECT IFNULL(FROMDATE,'1978-06-18') INTO dSTART; -- Если не указана начальная дата
+  SELECT IFNULL(TODATE,CURRENT_DATE) INTO dEND; -- Если не указана конечная дата
+
+   SELECT count(wp.ID) INTO iResult FROM pokaz wp where wp.PRID = ZAKAZPR and wp.STATE = POKAZSTATE and wp.POKAZTIME>=dSTART and wp.POKAZTIME<=dEND;
+   
+   SELECT IFNULL(iResult,-1) INTO iResult; -- На всякий случай :)
   RETURN iResult; 
 END$$
 DELIMITER ;
