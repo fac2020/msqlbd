05e0df966fef67d499202e2575e9f253b0712124
fac2020 <facquodvis@inbox.ru>
2017-09-29 13:05:48 +0400

    fix
    снятие метки приватного объекта при простановке статуса "наш объект"

--- trigger/prlog_AI.sql
+++ trigger/prlog_AI.sql
@@ -5,4 +5,8 @@ CREATE TRIGGER `prlog_AI` AFTER INSERT ON `prlog` FOR EACH ROW BEGIN
  UPDATE task t set t.ISCOMPLETE=1 where t.ID = NEW.PRID and t.AGENCYID=NEW.AGENCYID;
  -- Создаём событие "Смена статуса заявки Пр"
  call ADD_EVENT(18,NEW.ID,'',null);
+ IF (NEW.NEWSTATE = 19) THEN -- Если меняем на "Наш объект"
+   -- убираем метку "приватный объект", если объект принадлежит этой компании
+   update wrk_zakazpr wz set wz.PRIVATEOBJECT = 0 where wz.ID = NEW.PRID and wz.FIRMID = NEW.AGENCYID;
+ END IF;
 END
